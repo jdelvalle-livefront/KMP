e139642171df032ef7edd656e01eaf00dd219bc4
Juan del Valle <jdelvalle@livefront.com>
2026-09-13 14:23:51 -0500

    Initial config of the proyect

---
+++ shared/src/commonTest/kotlin/co/delvalle/artapp/SharedCommonTest.kt
@@ -0,0 +1,12 @@
+package co.delvalle.artapp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ shared/src/iosMain/kotlin/co/delvalle/artapp/MainViewController.kt
@@ -0,0 +1,5 @@
+package co.delvalle.artapp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
