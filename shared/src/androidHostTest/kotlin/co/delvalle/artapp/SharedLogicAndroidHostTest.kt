ebdb8847945cfaff752a4139366b314f8e5f592b
Juan del Valle <jdelvalle@livefront.com>
2026-09-13 14:23:51 -0500

    Initial config of the proyect

---
+++ shared/src/androidHostTest/kotlin/co/delvalle/artapp/SharedLogicAndroidHostTest.kt
@@ -0,0 +1,12 @@
+package co.delvalle.artapp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedLogicAndroidHostTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ shared/src/commonMain/kotlin/co/delvalle/artapp/Greeting.kt
@@ -0,0 +1,9 @@
+package co.delvalle.artapp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
